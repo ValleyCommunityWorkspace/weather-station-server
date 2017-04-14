dc93fa45ff87efcd4734f7b6791a473ae819a8f5
Ian Rees <ian.rees@gmail.com>
2017-04-14 21:51:54 +1200

    Add a landing page, just so we have one

--- weatherstation/weatherstation/urls.py
+++ weatherstation/weatherstation/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^weatherdata/', include('datareceiver.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('frontpage.urls')),
 ]
 
